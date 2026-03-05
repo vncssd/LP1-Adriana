239e2feedae3a7a81435188f8c343ebc0c099669
Vincius <viniviniee11@gmail.com>
2026-03-04 21:50:54 -0300

    feat: implementa o método gerar relatório, promoção e aumento na classe Main

--- exercicio-abstracao/src/loja/Main.java
+++ exercicio-abstracao/src/loja/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
         Loja loja = new Loja();
 
-        Produto produto1 = new Produto("Lenço umedecido", 10.99, 24);
-        Produto produto2 = new Produto("Paracetamol", 11.99, 13 );
+        Produto produto1 = new Produto("Lenço umedecido", 10.99, 24, 0);
+        Produto produto2 = new Produto("Paracetamol", 11.99, 13, 0);
 
         loja.setBalanco(0.0);
         loja.adicionarProduto(produto1);
@@ -19,6 +19,17 @@ public class Main {
         System.out.println("BALANÇO: " + loja.getBalanco());
         loja.venderProduto(produto1, 2);
         System.out.println("BALANÇO: " + loja.getBalanco());
+
+        produto1.promocao(50.0);
+        loja.venderProduto(produto1, 2);
+        System.out.println("BALANÇO: " + loja.getBalanco());
+        produto1.promocao(10.0);
+        produto2.aumento(20.0);
+
+        loja.listarProdutos();
+
+        System.out.println();
+        System.out.println(produto1.gerarRelatorio(produto1));
 
     }
 
