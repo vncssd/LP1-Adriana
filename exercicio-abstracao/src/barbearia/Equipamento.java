6baccc811b281dcabc7758d8ec7d2a8e2c4d6461
Vincius <viniviniee11@gmail.com>
2026-04-07 11:49:27 -0300

    feat: cria os métodos da classe equipamento

--- exercicio-abstracao/src/barbearia/Equipamento.java
+++ exercicio-abstracao/src/barbearia/Equipamento.java
@@ -41,6 +41,32 @@ public class Equipamento {
         this.marca = marca;
     }
 
+    public void setEquipamentoEstado(EquipamentoEstado equipamentoEstado) {
+        this.equipamentoEstado = equipamentoEstado;
+    }
+
+    public EquipamentoEstado getEquipamentoEstado() {
+        return equipamentoEstado;
+    }
+
+    public boolean buscarEquipamentoPorNome(Equipamento equipamento){
+        return equipamento.getMarca().toLowerCase().equals(getMarca().toLowerCase())
+                && equipamento.getModelo().toLowerCase().equals(getModelo().toLowerCase());
+    }
+
+    public void receberManutencao(){
+        setRecebeuManuntencao(true);
+    }
+
+    public void ligar(){
+        setEquipamentoEstado(EquipamentoEstado.LIGADO);
+        System.out.println(getMarca() + " " + getModelo() + " está " + getEquipamentoEstado());
+    }
+
+    public void desligar(){
+        setEquipamentoEstado(EquipamentoEstado.DESLIGADO);
+        System.out.println(getMarca() + " " + getModelo() + " está " + getEquipamentoEstado());
+    }
 
 
 }
