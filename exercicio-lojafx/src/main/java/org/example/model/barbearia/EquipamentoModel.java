e1008d4b945f5caf520b4284570b8ac65c5dc830
Vincius <viniviniee11@gmail.com>
2026-04-14 14:36:58 -0300

    feat: cria o model do equipamento da barbearia

---
+++ exercicio-lojafx/src/main/java/org/example/model/barbearia/EquipamentoModel.java
@@ -0,0 +1,74 @@
+package org.example.model.barbearia;
+
+import org.example.enums.barbearia.EquipamentoEstado;
+
+public class EquipamentoModel {
+
+    private boolean recebeuManuntencao;
+    private String modelo;
+    private String marca;
+    private EquipamentoEstado equipamentoEstado;
+
+    public EquipamentoModel() {
+    }
+
+    public EquipamentoModel(boolean recebeuManuntencao, String modelo, String marca, EquipamentoEstado equipamentoEstado) {
+        this.recebeuManuntencao = recebeuManuntencao;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.equipamentoEstado = equipamentoEstado;
+    }
+
+    public boolean isRecebeuManuntencao() {
+        return recebeuManuntencao;
+    }
+
+    public void setRecebeuManuntencao(boolean recebeuManuntencao) {
+        this.recebeuManuntencao = recebeuManuntencao;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setEquipamentoEstado(EquipamentoEstado equipamentoEstado) {
+        this.equipamentoEstado = equipamentoEstado;
+    }
+
+    public EquipamentoEstado getEquipamentoEstado() {
+        return equipamentoEstado;
+    }
+
+    public boolean buscarEquipamentoPorNome(EquipamentoModel equipamento){
+        return equipamento.getMarca().toLowerCase().equals(getMarca().toLowerCase())
+                && equipamento.getModelo().toLowerCase().equals(getModelo().toLowerCase());
+    }
+
+    public void receberManutencao(){
+        setRecebeuManuntencao(true);
+    }
+
+    public void ligar(){
+        setEquipamentoEstado(EquipamentoEstado.LIGADO);
+        System.out.println(getMarca() + " " + getModelo() + " está " + getEquipamentoEstado());
+    }
+
+    public void desligar(){
+        setEquipamentoEstado(EquipamentoEstado.DESLIGADO);
+        System.out.println(getMarca() + " " + getModelo() + " está " + getEquipamentoEstado());
+    }
+
+
+}
