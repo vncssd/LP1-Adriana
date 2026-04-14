153d44e20e7f8a9cc466ee12c971e3e3b0b557d4
Vincius <viniviniee11@gmail.com>
2026-04-14 13:36:49 -0300

    feat: cria o model do Produto da loja

---
+++ exercicio-lojafx/src/main/java/org/example/model/ProdutoModel.java
@@ -0,0 +1,67 @@
+package org.example.model;
+
+public class ProdutoModel {
+
+    private String nome;
+    private Double preco;
+    private Integer quantidadeEmEstoque;
+    private Integer quantidadeVendida;
+
+    public ProdutoModel() {
+    }
+
+    public ProdutoModel(String nome, Double preco, Integer quantidadeEmEstoque, Integer quantidadeVendida) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.quantidadeVendida = quantidadeVendida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(Integer quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public Integer getQuantidadeVendida() {
+        return quantidadeVendida;
+    }
+
+    public void setQuantidadeVendida(Integer quantidadeVendida) {
+        this.quantidadeVendida = quantidadeVendida;
+    }
+
+    public String gerarRelatorio(ProdutoModel produto){
+        return "PRODUTO: " + produto.getNome() +
+                "\nQUANTIDADE VENDIDA: " + produto.getQuantidadeVendida();
+    }
+
+    public void promocao(Double porcentagem){
+        setPreco(preco-(preco*(porcentagem*0.01)));
+    }
+
+    public void aumento(Double porcentagem){
+        setPreco(preco+(preco*(porcentagem*0.01)));
+    }
+
+
+
+}
